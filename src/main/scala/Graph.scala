34922f70b959153aa37340a008e049cb94faaeb7
Yanjie He <heyanjie0@outlook.com>
2020-11-30 16:01:28 -0500

    add comments for the two functions in the Graph module

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -9,9 +9,19 @@ trait Graph[V] {
 }
 
 object Graph {
+
+  /** Creates a directed graph based on an adjacency list
+    *
+    * @param adjList
+    * @return
+    */
   def apply[V](adjList: Map[V, List[V]]): Graph[V] =
     new DirectedGraph[V](adjList)
 
+  /** Creates an empty directed graph
+    *
+    * @return
+    */
   def apply[V](): Graph[V] =
     new DirectedGraph[V](Map[V, List[V]]())
 }
